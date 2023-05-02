ef7777cfa2398c8d12f8dc8bf1915cce72f2148a
Bush <bushuev2002-2013@yandex.ru>
2023-05-02 21:07:08 +0300

    first commit tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,15 +1,15 @@
 let a = document.querySelectorAll(".has-tooltip");
 for(let i = 0; i< a.length; i++){
-a[i].addEventListener('click',()=>{
-    if(document.querySelectorAll('.tooltip')=== null || document.querySelectorAll('.tooltip').length!= a.length){
-        a[i].href = "#"
-        a[i].insertAdjacentHTML('afterend','<div class="tooltip" data-position= "bottom"></div>')
-        let b = document.querySelectorAll('.tooltip')
-        b[i].classList.add("tooltip_active")
-        b[i].textContent = a[i].title
-    }else{
-        b[i].classList.remove('tooltip_active')
-        // console.log(1)
-    }
-})
+    a[i].href = "#"
+    a[i].insertAdjacentHTML('afterend','<div class="tooltip" style="left: 0; top: 0"></div>')
+    let b = document.querySelectorAll('.tooltip')
+    b[i].textContent = a[i].title
+    a[i].addEventListener('click',(e)=>{
+        b[i].style.cssText = `left: ${e.x}px;`
+        if(b[i].classList.contains('tooltip_active')){
+            b[i].classList.remove('tooltip_active')
+        }else{
+            b[i].classList.add('tooltip_active')
+        }
+    })
 }
