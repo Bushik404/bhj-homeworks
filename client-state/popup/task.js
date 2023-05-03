e8b4cf69b002d7c951128830c4b9ce864939fb9f
Bush <bushuev2002-2013@yandex.ru>
2023-05-03 13:27:09 +0300

    first commit popup

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,10 +1,9 @@
 let add = document.querySelector('.modal')
 let a = document.querySelector('.modal__close')
-if(document.cookie.indexOf('remove=true')===0){
-}else{
+if(document.cookie.indexOf('remove=true')===-1){
     add.classList.add('modal_active')
     a.addEventListener('click',()=>{
         add.classList.remove('modal_active')
         document.cookie = "remove=true"
-    })  
+    })
 }
